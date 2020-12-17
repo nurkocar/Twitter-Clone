334aa5a326bd315170401af9dfd407ed2988ea90
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-12-17 19:34:26 +0000

    Created the app

---
+++ components/layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LayoutExtra from './LayoutExtra';
+import LayoutMain from './LayoutMain';
+import LayoutSidebar from './LayoutSidebar';
+import styles from './Layout.module.css';
+
+const Layout = () => {
+    return <div></div>;
+};
+
+export default Layout;
---
+++ components/layout/LayoutSidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LayoutExtra from './LayoutExtra';
+import LayoutMain from './LayoutMain';
+import LayoutSidebar from './LayoutSidebar';
+
+const Layout = () => {
+    return <div></div>;
+};
+
+export default Layout;
